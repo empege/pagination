7a749f8aa9d60019f696edfe0adf4a7f91974b56
Ivan Mitov <ivan.mitov@hotmail.com>
2021-04-12 12:53:04 +0200

    123

--- paginacija.js
+++ paginacija.js
@@ -1,66 +1,67 @@
-const rootElement = document.getElementById('root');
+$(function () {
 
-// Za sad radi samo ako ima 20 stranica i vise
-const numberOfPages = 20;
-const pagesBeforeDivision = 5; // At what page does it start to show 1...5...9 10 11...15...20 (before it's 1 2 3 4 pagesBeforeDivision(5)...lastPage)
-let currentPage = 10;
-
-const paginationEllipsis = '<span class="m-pagination__ellipsis">... </span>';
-const paginationClass = 'm-pagination__page-number';
-
-const createPagination = (numberOfPages, currentPage) => {
-  const paginationElement = document.createElement('DIV');
-  paginationElement.classList.add('m-pagination');
-  const paginationContentsWrapper = document.createElement('DIV');
-  paginationContentsWrapper.classList.add('m-pagination__contents-wrapper');
-
-  let paginationContents = '';
-
-  if (numberOfPages >= 20) {
-    // If less than DIVISION number
-    if (currentPage < pagesBeforeDivision) {
-      for (let i = 1; i <= pagesBeforeDivision; i++) {
-        paginationContents += `<span class="${paginationClass} ${i === currentPage ? `${paginationClass}--active` : ''}" data-page-number="${i}">${i}</span>`;
-      }
-      paginationContents += `${paginationEllipsis}<span class="${paginationClass}" data-page-number="${numberOfPages}">${numberOfPages}</span>`;
-      // If more than LAST - DIVISION number
-    } else if (currentPage > numberOfPages - pagesBeforeDivision + 1) {
-      paginationContents += `<span class="${paginationClass}" data-page-number="1">1</span>${paginationEllipsis}`;
-      for (let i = numberOfPages - pagesBeforeDivision + 1; i <= numberOfPages; i++) {
-        paginationContents += `<span class="${paginationClass} ${i === currentPage ? `${paginationClass}--active` : ''}" data-page-number="${i}">${i}</span>`;
-      }
-      // In BETWEEN
-    } else {
-      paginationContents += `
-      <span class="${paginationClass}" data-page-number="1">1</span>
-      ${paginationEllipsis}
-      <span class="${paginationClass}" data-page-number="${Math.round((currentPage - 1) / 2)}">${Math.round((currentPage - 1) / 2)}</span>
-      ${paginationEllipsis}
-      <span class="${paginationClass}" data-page-number="${currentPage - 1}">${currentPage - 1}</span>
-      <span class="${paginationClass} m-pagination__page-number--active" data-page-number="${currentPage}">${currentPage}</span>
-      <span class="${paginationClass}" data-page-number="${currentPage + 1}">${currentPage + 1}</span>
-      ${paginationEllipsis}
-      <span class="${paginationClass}" data-page-number="${Math.round((numberOfPages - currentPage) / 2 + currentPage)}">${Math.round((numberOfPages - currentPage) / 2 + currentPage)}</span>
-      ${paginationEllipsis}
-      <span class="${paginationClass}" data-page-number="${numberOfPages}">${numberOfPages}</span>`;
+  // Selectors
+  const pagination = {
+    selectors: {
+    },
+    stateClasses: {
+      pagination: '.m-pagination',
+      pageNumber: 'm-pagination__page-number',
     }
-    paginationContentsWrapper.innerHTML = paginationContents;
   }
 
-  paginationElement.append(paginationContentsWrapper);
-  // Verovatno ne bi trebalo ovo da radim rootu, nego nekom wrapperu koji tu stoji bas za paginaciju...
-  rootElement.innerHTML = '';
-  rootElement.append(paginationElement);
+  // Elements
+  const paginationElement = $(pagination.stateClasses.pagination);
 
-  // Add event listeners (nisam stavljao js selektore, samo sam ovako direktno za sad)
-  const pageNumbers = document.querySelectorAll(`.${paginationClass}`);
+  // Variables
+  const numberOfPages = 20;
+  const pagesBeforeDivision = 5; // DIVISION number - At what page does it start to show 1...5...9 10 11...15...20 (before it's 1 2 3 4 pagesBeforeDivision(5)...lastPage)
+  let currentPage = 10;
 
-  pageNumbers.forEach(current => {
-    current.addEventListener('click', (e) => {
-      const currentPage = Number(e.target.dataset.pageNumber);
-      console.log(e.target.dataset.pageNumber);
+  // Render Pagination Function
+  const createPagination = (numberOfPages, currentPage) => {
+    const paginationContents = $('<div class="m-pagination__contents"></div>');
+
+    if (numberOfPages >= 20) {
+      // If less than DIVISION number
+      if (currentPage < pagesBeforeDivision) {
+        for (let i = 1; i <= pagesBeforeDivision; i++) {
+          paginationContents.append(`<span class="${pagination.stateClasses.pageNumber} ${i === currentPage ? `${pagination.stateClasses.pageNumber}--active` : ''}" data-page-number="${i}">${i}</span>`);
+        }
+        paginationContents.append(`...<span class="${pagination.stateClasses.pageNumber}" data-page-number="${numberOfPages}">${numberOfPages}</span>`);
+        // If more than LAST - DIVISION number
+      } else if (currentPage > numberOfPages - pagesBeforeDivision + 1) {
+        paginationContents.append(`<span class="${pagination.stateClasses.pageNumber}" data-page-number="1">1</span>...`);
+        for (let i = numberOfPages - pagesBeforeDivision + 1; i <= numberOfPages; i++) {
+          paginationContents.append(`<span class="${pagination.stateClasses.pageNumber} ${i === currentPage ? `${pagination.stateClasses.pageNumber}--active` : ''}" data-page-number="${i}">${i}</span>`);
+        }
+        // In BETWEEN
+      } else {
+        paginationContents.html(`
+          <span class="${pagination.stateClasses.pageNumber}" data-page-number="1">1</span>
+          ...
+          <span class="${pagination.stateClasses.pageNumber}" data-page-number="${Math.round((currentPage - 1) / 2)}">${Math.round((currentPage - 1) / 2)}</span>
+          ...
+          <span class="${pagination.stateClasses.pageNumber}" data-page-number="${currentPage - 1}">${currentPage - 1}</span>
+          <span class="${pagination.stateClasses.pageNumber} m-pagination__page-number--active" data-page-number="${currentPage}">${currentPage}</span>
+          <span class="${pagination.stateClasses.pageNumber}" data-page-number="${currentPage + 1}">${currentPage + 1}</span>
+          ...
+          <span class="${pagination.stateClasses.pageNumber}" data-page-number="${Math.round((numberOfPages - currentPage) / 2 + currentPage)}">${Math.round((numberOfPages - currentPage) / 2 + currentPage)}</span>
+          ...
+          <span class="${pagination.stateClasses.pageNumber}" data-page-number="${numberOfPages}">${numberOfPages}</span>
+        `)
+      }
+    }
+    // Reset contents and rerender
+    paginationElement.empty();
+    paginationElement.append(paginationContents);
+
+    // Event listener for pagination numbers (Ovo moze gore jedino ako imamo neki placeholder vec u htmlu pa da moze da ga nadje (jer inace ne postoje pageNumberi dok se ne naprave u jsu iznad), i taman da ima nesto dok loaduje npr?)
+    const pageNumbers = $(`.${pagination.stateClasses.pageNumber}`);
+    pageNumbers.on('click', function () {
+      const currentPage = $(this).data('pageNumber');
       createPagination(numberOfPages, currentPage)
-    });
-  })
-}
-createPagination(numberOfPages, currentPage);
+    })
+  }
+  createPagination(numberOfPages, currentPage);
+});
